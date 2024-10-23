bbe545117f3569443d30f4ef8139f995c7b3e61e
ASCorreia <36857753+ASCorreia@users.noreply.github.com>
2024-10-23 16:08:43 +0100

    Initial Commit

---
+++ programs/anchor-escrow/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("FircrADQ2wgGuvpm8qneNCfKM7o5zoHTWnDQxngpTQ3J");
+
+#[program]
+pub mod anchor_escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
